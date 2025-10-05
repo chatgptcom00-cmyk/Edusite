a3a579e56d43c1f7aead895a0f9397903cdff7f6
Yansh Uuu <chatgpt.com00@gmail.com>
2025-10-05 13:59:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-238336437-7014a",
+  "appId": "1:552603188750:web:3bea331a17b837fec0c65f",
+  "apiKey": "AIzaSyByrUUW_Xs0vzIWHT54y7QwFV_FLpa6ttI",
+  "authDomain": "studio-238336437-7014a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "552603188750"
+};
